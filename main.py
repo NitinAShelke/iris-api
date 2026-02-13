6becf9c6e6a139df4d20991a1eb1ff810ffa84aa
Nitin <nitinashelke@gmail.com>
2026-02-13 10:51:40 +0530

    Added frontend UI

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 from fastapi import FastAPI
 import joblib
+from fastapi import Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 # Load model
 model = joblib.load("iris_rf_model.pkl")
 
 classes = ["Setosa", "Versicolor", "Virginica"]
 
-@app.get("/")
-def home():
-    return {"message": "Iris Classification API is running"}
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/predict")
-def predict(sepal_length: float,
-            sepal_width: float,
-            petal_length: float,
-            petal_width: float):
 
-    prediction = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
-    result = classes[prediction[0]]
+@app.post("/predict-ui", response_class=HTMLResponse)
+def predict_ui(
+    request: Request,
+    sepal_length: float = Form(...),
+    sepal_width: float = Form(...),
+    petal_length: float = Form(...),
+    petal_width: float = Form(...)
+):
+    input_data = [[sepal_length, sepal_width, petal_length, petal_width]]
+    prediction = model.predict(input_data)[0]
 
-    return {"Predicted Flower": result}
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "prediction": prediction
+    })
